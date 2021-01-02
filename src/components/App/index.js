07f37a826f6d8cf9f29a25ee8cc56551480c1616
yeg2799 <yeg2799@gmail.com>
2021-01-02 13:17:49 +0300

    deleted node_modules

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
 import "../../styles/index.scss";
 import Header from "../Header/Header";
-import Content from '../CenterContent/Content';
-import Filter from '../CenterContent/Filter/Filter';
-import { Switch,Route } from "react-router-dom";
+import Content from "../CenterContent/Content";
+import Filter from "../CenterContent/Filter/Filter";
+import { Switch, Route } from "react-router-dom";
 import Aboutme from "../CenterContent/Aboutme";
 import Episodes from "../CenterContent/Episodes";
 export default class index extends Component {
   state = {
     characterUrl: "https://rickandmortyapi.com/api/character",
-    charactersResults:[],
-    characterInfo:[],
-    filterCharacters:[],
-    episodes:[],
-    episodeInfo:[],
-    episodeCharacters:[],
-    url:"https://rickandmortyapi.com/api/character?page=",
-    urlEpisode:"https://rickandmortyapi.com/api/episode",
-    currentName:"",
-    currentGender:"",
-    currentStatus:"",
-    boolFilter:false
+    charactersResults: [],
+    characterInfo: [],
+    filterCharacters: [],
+    episodes: [],
+    episodeInfo: [],
+    episodeCharacters: [],
+    url: "https://rickandmortyapi.com/api/character?page=",
+    urlEpisode: "https://rickandmortyapi.com/api/episode",
+    currentName: "",
+    currentGender: "",
+    currentStatus: "",
+    boolFilter: false,
   };
-  onChangeHandle=(e)=>{
-    this.setState({[e.target.name]:e.target.value})
-  }
-  componentDidMount(){
+  onChangeHandle = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  componentDidMount() {
     this.getCharacters(this.state.url);
     this.getCharacterInfo(this.state.url);
     this.getAllEpisodes(this.state.urlEpisode);
@@ -34,76 +34,97 @@ export default class index extends Component {
     this.getFilterCharacters();
   }
   //Get Character
-  getCharacters=(url)=>{
+  getCharacters = (url) => {
     fetch(url)
-    .then(response=>response.json())
-    .then(data=>this.setState({charactersResults:data.results}))
-    .catch(err=>console.log(err));
-  }
+      .then((response) => response.json())
+      .then((data) => this.setState({ charactersResults: data.results }))
+      .catch((err) => console.log(err));
+  };
   //Character info (page vs)
-  getCharacterInfo=(url)=>{
+  getCharacterInfo = (url) => {
     fetch(url)
-    .then(response=>response.json())
-    .then(data=>this.setState({characterInfo:data.info}))
-    // .then(data=>console.log(data.info.next))
-    .catch(err=>console.log(err));
-  }
+      .then((response) => response.json())
+      .then((data) => this.setState({ characterInfo: data.info }))
+      // .then(data=>console.log(data.info.next))
+      .catch((err) => console.log(err));
+  };
   //Filter Characters
-  getFilterCharacters=(name,gender,status)=>{
-    let url="https://rickandmortyapi.com/api/character/";
-    if(name || gender || status){
-      this.setState({boolFilter:true});
-     url+= `?name=${name}&gender=${gender}&status=${status}`;
+  getFilterCharacters = (name, gender, status) => {
+    let url = "https://rickandmortyapi.com/api/character/";
+    if (name || gender || status) {
+      this.setState({ boolFilter: true });
+      url += `?name=${name}&gender=${gender}&status=${status}`;
     }
     fetch(url)
-    .then(response=>response.json())
-    .then(data=>this.setState({filterCharacters:data.results}))
-    // .then(data=>console.log(data.results))
-    .catch(err=>console.error(err));
-  }
+      .then((response) => response.json())
+      .then((data) => this.setState({ filterCharacters: data.results }))
+      // .then(data=>console.log(data.results))
+      .catch((err) => console.error(err));
+  };
   //getAllEpisodes
-  getAllEpisodes=(urlEpisode)=>{
+  getAllEpisodes = (urlEpisode) => {
     fetch(urlEpisode)
-    .then(response=>response.json())
-    .then(data=>this.setState({episodes:data.results}))
-    // .then(data=>console.log(data))
-    .catch(err=>console.error(err));
-  }
+      .then((response) => response.json())
+      .then((data) => this.setState({ episodes: data.results }))
+      // .then(data=>console.log(data))
+      .catch((err) => console.error(err));
+  };
   //Epispode Info
-  getEpisodesInfo=(urlEpisode)=>{
+  getEpisodesInfo = (urlEpisode) => {
     fetch(urlEpisode)
-    .then(response=>response.json())
-    .then(data=>this.setState({episodeInfo:data.info}))
-    .catch(err=>console.log(err));
-  }
-  getEpisodesCharacters=(urlEpisode)=>{
+      .then((response) => response.json())
+      .then((data) => this.setState({ episodeInfo: data.info }))
+      .catch((err) => console.log(err));
+  };
+  getEpisodesCharacters = (urlEpisode) => {
     fetch(urlEpisode)
-    .then(response=>response.json())
-    // .then(data=>this.setState({episodeInfo:data.result.characters}))
-    .then(data=>console.log(data.results))
-    .catch(err=>console.log(err));
-  }
+      .then((response) => response.json())
+      // .then(data=>this.setState({episodeInfo:data.result.characters}))
+      .then((data) => console.log(data.results))
+      .catch((err) => console.log(err));
+  };
   render() {
     return (
       <div class="body">
-         {/*Part Header */}
-         <div class="part-header">
-           <Header></Header>
-         </div>
+        {/*Part Header */}
+        <div class="part-header">
+          <Header></Header>
+        </div>
         <div class="container">
-             {/*Part Content */}
+          {/*Part Content */}
           <div class="row partCenter">
             <Switch>
               <Route exact path="/">
-              <div class="col-4 filter ">
-              <Filter filter={this.getFilterCharacters} currentName={this.state.currentName} currentGender={this.state.currentGender} currentStatus={this.state.currentStatus} onChangeHandle={this.onChangeHandle}></Filter>
-            </div>
-            <div class="col-8 ">  
-             <Content boolFilter={this.state.boolFilter} getCharacters={this.state.charactersResults} filterCharacters={this.state.filterCharacters} info={this.state.characterInfo} characters={this.getCharacters} url={this.state.url} infos={this.getCharacterInfo}></Content>
-            </div>
+                <div class="col-4 filter">
+                  <Filter
+                    filter={this.getFilterCharacters}
+                    currentName={this.state.currentName}
+                    currentGender={this.state.currentGender}
+                    currentStatus={this.state.currentStatus}
+                    onChangeHandle={this.onChangeHandle}
+                  ></Filter>
+                </div>
+                <div class="col-8">
+                  <Content
+                    boolFilter={this.state.boolFilter}
+                    getCharacters={this.state.charactersResults}
+                    filterCharacters={this.state.filterCharacters}
+                    info={this.state.characterInfo}
+                    characters={this.getCharacters}
+                    url={this.state.url}
+                    infos={this.getCharacterInfo}
+                  ></Content>
+                </div>
               </Route>
               <Route exact path="/episodes">
-               <Episodes getEpisodesInfo={this.getEpisodesInfo} getAllEpisodes={this.getAllEpisodes} episodes={this.state.episodes} episodeInfo={this.state.episodeInfo} > </Episodes>
+                <Episodes
+                  getEpisodesInfo={this.getEpisodesInfo}
+                  getAllEpisodes={this.getAllEpisodes}
+                  episodes={this.state.episodes}
+                  episodeInfo={this.state.episodeInfo}
+                >
+                  {" "}
+                </Episodes>
               </Route>
               <Route exact path="/about">
                 <Aboutme></Aboutme>
@@ -111,7 +132,6 @@ export default class index extends Component {
             </Switch>
           </div>
         </div>
-        
       </div>
     );
   }
--- src/components/Header/SocialMedia.js
+++ src/components/Header/SocialMedia.js
@@ -11,62 +11,60 @@ export default class SocialMedia extends Component {
     return (
       <div class="social mt-5">
         <div class="row">
-          <div class="col-4">
-          </div>
+          <div class="col-4"></div>
           <div class="col-8 social-m">
-          <div class="col-2">
-            <a
-              href="https://www.facebook.com/emre.guzel.3386/"
-              target="_blank"
-              rel="noreferrer"
-              class="face"
-            >
-              <FaFacebook class="i"></FaFacebook>
-            </a>
+            <div class="col-2">
+              <a
+                href="https://www.facebook.com/emre.guzel.3386/"
+                target="_blank"
+                rel="noreferrer"
+                class="face"
+              >
+                <FaFacebook class="i"></FaFacebook>
+              </a>
+            </div>
+            <div class="col-2">
+              <a
+                href="https://www.instagram.com/emregzl27/"
+                target="_blank"
+                rel="noreferrer"
+                class="insta"
+              >
+                <FaInstagram class="i"></FaInstagram>
+              </a>
+            </div>
+            <div class="col-2">
+              <a
+                href="https://www.linkedin.com/in/emre-g%C3%BCzel-6bb4101a2/"
+                target="_blank"
+                rel="noreferrer"
+                class="linkedin"
+              >
+                <FaLinkedinIn class="i"></FaLinkedinIn>
+              </a>
+            </div>
+            <div class="col-2">
+              <a
+                href="https://github.com/yeg2799"
+                target="_blank"
+                rel="noreferrer"
+                class="github"
+              >
+                <FaGithub class="i"></FaGithub>
+              </a>
+            </div>
+
+            <div class="col-2">
+              <a
+                href="https://twitter.com/emrex99"
+                target="_blank"
+                rel="noreferrer"
+                class="twitter"
+              >
+                <FaTwitter class="i"></FaTwitter>
+              </a>
+            </div>
           </div>
-          <div class="col-2">
-            <a
-              href="https://www.instagram.com/emregzl27/"
-              target="_blank"
-              rel="noreferrer"
-              class="insta"
-            >
-              <FaInstagram class="i"></FaInstagram>
-            </a>
-          </div>
-          <div class="col-2">
-            <a
-              href="https://www.linkedin.com/in/emre-g%C3%BCzel-6bb4101a2/"
-              target="_blank"
-              rel="noreferrer"
-              class="linkedin"
-            >
-              <FaLinkedinIn class="i"></FaLinkedinIn>
-            </a>
-          </div>
-          <div class="col-2">
-            <a
-              href="https://github.com/yeg2799"
-              target="_blank"
-              rel="noreferrer"
-              class="github"
-            >
-              <FaGithub class="i"></FaGithub>
-            </a>
-          </div>
-          
-          <div class="col-2">
-            <a
-              href="https://twitter.com/emrex99"
-              target="_blank"
-              rel="noreferrer"
-              class="twitter"
-            >
-              <FaTwitter class="i"></FaTwitter>
-            </a>
-          </div>
-          </div>
-         
         </div>
       </div>
     );
